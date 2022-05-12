1cbe554b4d602bc7ddb895869a64f5b89756a469
Sparsh09 <sparsh1698.cse18@chitkara.edu.in>
2022-05-12 23:54:56 +0530

    added destroy method

--- StartStopPauseUpdate.sol
+++ StartStopPauseUpdate.sol
@@ -5,25 +5,38 @@ contract StartStopUpdate{
 
     address public owner;
     bool paused;
-
+    bool public destroyed;
 
     constructor() {
         owner = msg.sender;
     }
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "Only Owner has the permission");
+        _;
+    } 
+
+    modifier isDestroyed{
+        require(destroyed != true , "The contract is destroyed");
+        _;
+    }
     
-    function sendMoney() public payable{
+    function sendMoney() public payable isDestroyed{
 
     }
 
-    function setPaused(bool _paused) public {
-        require(msg.sender == owner , "Owner can do it");
+    function setPaused(bool _paused) public onlyOwner isDestroyed{
         paused = _paused; 
     }
 
-    function withdrawAllMoney(address payable _to) public {
-        require(owner == msg.sender , "Owner can send money");
+    function withdrawAllMoney(address payable _to) public onlyOwner isDestroyed{
         require(paused == true , "The Contract is on Paused Mode");
         _to.transfer(address(this).balance);
+    }
+
+    function stop(address payable _to) public onlyOwner isDestroyed{
+        destroyed = true;
+        selfdestruct(_to);
     }
 
 }
